f9243f4156b5b95ebcc4e6dbf1682d8515ea87b5
nurik <nurik6@gmail.com>
2021-03-24 20:24:29 +0500

    feat: final

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import { Command, flags } from '@oclif/command';
 import * as path from 'path';
 import * as fs from 'fs';
-import * as tar from 'tar';
-import * as minimatch from 'minimatch';
 import * as zlib from 'zlib';
 import * as tarFs from 'tar-fs';
+import * as micromatch from 'micromatch';
+import { convertBytesToHuman, calculateCompressRatio } from './utils';
 
 class ProjectsBackuper extends Command {
 	static description = 'Backup dir with progress and ignore syntax';
 
 	static flags = {
-		version: flags.version({ char: 'v' }),
+		version: flags.version(),
+		verbose: flags.boolean({
+			char: 'v',
+			description: 'verbose',
+			default: false
+		}),
+		silent: flags.boolean({
+			description: 'do not output info to stdout',
+			default: false
+		}),
 		help: flags.help({ char: 'h' }),
 		from: flags.string({
 			description: 'source dir, wich used to archive',
@@ -19,6 +28,15 @@ class ProjectsBackuper extends Command {
 		to: flags.string({
 			description: 'destination archive path',
 			required: true
+		}),
+		exclude: flags.string({
+			description: `skip files or dirs that should not be packed
+				see https://www.npmjs.com/package/micromatch for pattern details`,
+			multiple: true
+		}),
+		gzip: flags.boolean({
+			description: 'enable gzip compression',
+			default: false
 		})
 	};
 
@@ -27,35 +45,86 @@ class ProjectsBackuper extends Command {
 	async run() {
 		const { flags } = this.parse(ProjectsBackuper);
 
+		const enableGzip = flags.gzip;
 		const from = path.resolve(flags.from);
 		const targetDirName = path.basename(from);
 		const archiveName = path.basename(flags.to);
 		const archiveDir = path.join(path.dirname(flags.to));
 		const archivePath = path.resolve(path.join(archiveDir, './tmp', archiveName));
 
-		this.log(`Creating "${archiveName}"`);
-		this.log(`from "${from}"`);
-		this.log(`to ${archivePath}`);
+		const verbose = flags.verbose;
+		const silent = flags.silent;
+
+		const excludedEntries = flags.exclude;
+
+		let totalBytes: number = 0;
+		let totalCompressedBytes: number = 0;
+		if (!silent) {
+			this.log(`Creating "${archiveName}"`);
+			this.log(`from "${from}"`);
+			this.log(`to ${archivePath}`);
+		}
+
+		if (verbose && !silent) {
+			this.log(`gzip compression enabled for "${archiveName}"`);
+			if (excludedEntries) {
+				this.log(`Ignored paths: `, excludedEntries);
+			}
+		}
 
 		const normalizedPath = archivePath;
 		if (!hasExtension(normalizedPath)) {
 			this.error(" --to should contain extension: 'tar' 'gz' etc in a path");
 		}
+		const tarOptions: tarFs.PackOptions = {
+			map: (header) => {
+				header.name = `${targetDirName}${path.sep}${header.name}`;
+				if (!silent) {
+					if (verbose) {
+						this.log(`${header.name} ${header.size}`);
+					} else {
+						this.log(header.name);
+					}
+				}
+				return header;
+			}
+		};
 
-		await tar.c(
-			{
-				gzip: true,
-				file: archivePath,
-				C: '',
-				preservePaths: false,
-				filter: (mpath, stat) => {
-					this.log(mpath);
-					return !minimatch(mpath, 'node_modules', { matchBase: true });
-				},
-				noPax: true
-			},
-			[from]
-		);
+		if (excludedEntries && excludedEntries.length > 0) {
+			tarOptions.ignore = (mpath) => {
+				return excludedEntries.some((pattern) => micromatch.isMatch(mpath, pattern));
+			};
+		}
+
+		const archive = tarFs.pack(from, tarOptions);
+
+		archive.on('data', (chunk: any) => {
+			totalBytes += chunk.length;
+		});
+
+		const output = fs.createWriteStream(normalizedPath);
+
+		if (enableGzip) {
+			const gzipStream = archive.pipe(zlib.createGzip());
+			gzipStream.on('data', (chunk) => {
+				totalCompressedBytes += chunk.length;
+			});
+			gzipStream.pipe(output).on('close', () => {
+				if (!silent && verbose) {
+					this.log(`Uncompressed archive size: ${convertBytesToHuman(totalBytes, 2)}`);
+					this.log(`Total compression ratio: ${calculateCompressRatio(totalBytes, totalCompressedBytes)}%`);
+				}
+				if (!silent) {
+					this.log(`Total archive size: ${convertBytesToHuman(totalCompressedBytes, 2)}`);
+				}
+			});
+		} else {
+			archive.pipe(output).on('close', () => {
+				if (!silent) {
+					this.log(`Total archive size: ${convertBytesToHuman(totalBytes, 2)}`);
+				}
+			});
+		}
 	}
 }
 
---
+++ src/utils.ts
@@ -0,0 +1,31 @@
+export function convertBytesToHuman(
+	bytes: number,
+	precision: number = 2,
+	type?: 'b' | 'kb' | 'mb' | 'gb' | 'zb' | 'pb'
+): string {
+	if (bytes < 1024) {
+		return `${bytes} b`;
+	}
+	let remain: [number, string] = [bytes, 'b'];
+	let counter = 0;
+	let types = ['b', 'kb', 'mb', 'gb', 'zb', 'pb'];
+
+	const hasType = !!type;
+
+	while (remain[0] >= 1024 && counter < types.length) {
+		remain = [remain[0] / 1024, types[++counter]];
+		if (hasType && type == remain[1]) {
+			break;
+		}
+	}
+
+	const tmpNum = parseInt(`1${'0'.repeat(precision)}`);
+
+	const result = Math.round((remain[0] + Number.EPSILON) * tmpNum) / tmpNum;
+
+	return `${result} ${remain[1]}`;
+}
+
+export function calculateCompressRatio(uncompressedSize: number, compressedSize: number): number {
+	return Math.round((((uncompressedSize - compressedSize) * 100) / uncompressedSize + Number.EPSILON) * 100) / 100;
+}
